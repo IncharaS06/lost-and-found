44cd7845b5040690c020aac7fdcd45862bebc8c3
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2026-02-03 13:46:19 +0530

    Delete app/lib directory

--- app/lib/testFirestore.ts
+++
@@ -1,9 +0,0 @@
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { db } from "./firebase";
-
-export async function testWrite(uid: string) {
-  await addDoc(collection(db, "test"), {
-    uid,
-    createdAt: serverTimestamp(),
-  });
-}
